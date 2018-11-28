ed604d5645a0b49e2f38dc338d349b92d08c3d30
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-11-28 17:35:16 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	var prefix = "$";
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```Supply A Number ًں‘Œ```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```Cleard: " + textxt + "\n Messages```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
+client.login('NTE3MzYxNDU3MzE1NDQ2Nzk1.DuBGbQ.Aa7w095ZVORW8Kl7P4w-pz21mYU');
